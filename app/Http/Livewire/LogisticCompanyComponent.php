928bb01eaec676d460cf77d56e4e907f36d89eb0
jose perez <joserph.a@gmail.com>
2020-11-11 21:15:36 -0500

    crud logisticcompany

--- app/Http/Livewire/LogisticCompanyComponent.php
+++ app/Http/Livewire/LogisticCompanyComponent.php
@@ -45,8 +45,76 @@ class LogisticCompanyComponent extends Component
 
         session()->flash('create', 'La empresa de logística "' . $logistic->name . '" se creo con éxito');
 
-        //$this->edit($logistic->id);
+        $this->edit($logistic->id);
     }
 
+    public function edit($id)
+    {
+        $logistic = LogisticCompany::find($id);
 
+        $this->logistic_id = $logistic->id;
+        $this->name = $logistic->name;
+        $this->ruc = $logistic->ruc;
+        $this->phone = $logistic->phone;
+        $this->address = $logistic->address;
+        $this->state = $logistic->state;
+        $this->city = $logistic->city;
+        $this->country = $logistic->country;
+
+        $this->view = 'edit';
+    }
+
+    public function update()
+    {
+        // Validaniones
+        $this->validate([
+            'name'      => 'required',
+            'ruc'       => 'required',
+            'phone'     => 'required|alpha_num',
+            'address'   => 'required',
+            'state'     => 'required',
+            'city'      => 'required',
+            'country'   => 'required',
+        ]);
+
+        $logistic = LogisticCompany::find($this->logistic_id);
+
+        $logistic->update([
+            'name'          => $this->name,
+            'ruc'           => $this->ruc,
+            'phone'         => $this->phone,
+            'address'       => $this->address,
+            'state'         => $this->state,
+            'city'          => $this->city,
+            'country'       => $this->country,
+            'id_user'       => $logistic->id_user,
+            'update_user'   => Auth::user()->id
+        ]);
+
+        session()->flash('edit', 'La empresa de logística "' . $logistic->name . '" se actualizó con éxito');
+
+        $this->default();
+    }
+
+    public function default()
+    {
+        $this->name = '';
+        $this->ruc = '';
+        $this->phone = '';
+        $this->address = '';
+        $this->state = '';
+        $this->city = '';
+        $this->country = '';
+
+        $this->view = 'create';
+    }
+
+    public function destroy($id)
+    {
+        $logistic = LogisticCompany::find($id);
+
+        $logistic->delete();
+
+        session()->flash('delete', 'La empresa de logística se eliminó con éxito');
+    }
 }
