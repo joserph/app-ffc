488485078493383531485529cab12035a0563861
jose perez <joserph.a@gmail.com>
2020-11-21 13:31:13 -0500

    upload to heroku

--- app/Http/Controllers/InvoiceHeaderController.php
+++ app/Http/Controllers/InvoiceHeaderController.php
@@ -23,7 +23,8 @@ class InvoiceHeaderController extends Controller
      */
     public function create()
     {
-        return view('masterinvoice.formHeader');
+        return view('masterinvoice.formHeader'); // prueba para subir app a heroku
+        
     }
 
     /**
