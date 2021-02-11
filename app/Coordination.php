f889da41651d1e3507a49c627becd7a2ba17fec2
Jose Perez FFC <joserph.a@gmail.com>
2021-02-11 15:27:51 -0500

    add migration coordination

---
+++ app/Coordination.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coordination extends Model
+{
+    protected $fillable = [
+        'hawb',
+        'pieces',
+        'hb',
+        'qb', 
+        'eb', 
+        'hb_r',
+        'qb_r',
+        'eb_r',
+        'missing',
+        'id_client',
+        'id_farm',
+        'id_load',
+        'variety_id',
+        'id_user',
+        'update_user'
+    ];
+}
