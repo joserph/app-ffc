dc5e63572bb9ffd2b5733f7c176da950958b7f54
Jose Perez FFC <joserph.a@gmail.com>
2021-03-31 15:34:55 -0500

    chage date in isf

--- resources/views/isf/isfPdf.blade.php
+++ resources/views/isf/isfPdf.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ISF - {{ $load->bl }} - {{ date('m/d/Y', strtotime($load->date)) }}</title>
+    <title>ISF - {{ $load->bl }} - {{ date('m-d-Y', strtotime($load->date)) }}</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
