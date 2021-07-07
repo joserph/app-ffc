5f1107be02b28100cb076fdb755cddf2f550787b
Jose Perez FFC <joserph.a@gmail.com>
2021-07-07 15:11:47 -0500

    change height in confirmation

--- resources/views/masterinvoice/shiptmentConfirmationInternalUsePdf.blade.php
+++ resources/views/masterinvoice/shiptmentConfirmationInternalUsePdf.blade.php
@@ -45,11 +45,11 @@
         }
         table.sinb, th{
             border: 1px solid black;
-            height: 15px;
+            height: 14px;
         }
         table.sinb, td{
             border: 1px solid black;
-            height: 13px;
+            height: 12px;
         }
         .text-white{
             color: #fff;
@@ -59,7 +59,7 @@
             border-right: 1px solid white;
             border-bottom: 1px solid black;
             border-left: 1px solid white;
-            height: 5px;
+            height: 3px;
         }
         .box-size{
             width: 40px;
