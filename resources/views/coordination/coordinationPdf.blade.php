9d353e87d44bc36c06de7e526f7521616c14a995
Jose Perez FFC <joserph.a@gmail.com>
2021-09-20 15:20:43 -0500

    add # in code load

--- resources/views/coordination/coordinationPdf.blade.php
+++ resources/views/coordination/coordinationPdf.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>COORDINACION MARITIMO {{ $load->bl }} - {{ $load->shipment }}</title>
+    <title>COORDINACION MARITIMO {{ $load->bl }} - #{{ $load->shipment }}</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
@@ -89,7 +89,7 @@
 <body>
     <table>
         <tr>
-            <th class="medium-letter">COORDINACIONES MARITIMO - {{ $load->bl }} - {{ $load->shipment }}</th>
+            <th class="medium-letter">COORDINACIONES MARITIMO - {{ $load->bl }} - #{{ $load->shipment }}</th>
         </tr>
     </table>
     
