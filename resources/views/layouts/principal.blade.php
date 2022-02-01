8cdcac79620a884752f0ac941bf78e7041ce813d
jose perez <joserph.a@gmail.com>
2022-02-01 15:59:09 -0500

    change in text on login and register

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -37,11 +37,11 @@
             <!-- Authentication Links -->
             @guest
                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link" href="{{ route('login') }}"><!--{{ __('Login') }}--> Iniciar Sesión</a>
                </li>
                @if (Route::has('register'))
                   <li class="nav-item">
-                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                     <a class="nav-link" href="{{ route('register') }}"><!--{{ __('Register') }}--> Registrar</a>
                   </li>
                @endif
             @else
